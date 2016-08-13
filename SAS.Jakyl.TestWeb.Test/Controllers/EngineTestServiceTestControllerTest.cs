7e39f1e9d9787a5345feb08bd5ee83586b391e5b
Sam Sussman <sussmansa@gmail.com>
2016-08-13 00:07:57 -0500

    Added tests and actions to demonstrate testing the Umbraco contentservice calls with the Umbraco Unit Test Engine.

---
+++ SAS.Jakyl.TestWeb.Test/Controllers/EngineTestServiceTestControllerTest.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SAS.Jakyl;
+using SAS.Jakyl.TestWeb.Controllers;
+using SAS.Jakyl.Core;
+using Umbraco.Core.Models;
+using Moq;
+
+namespace SAS.Jakyl.TestWeb.Test.Controllers
+{
+
+    [TestClass]
+    public class EngineTestServiceTestControllerTest
+    {
+
+        private UmbracoUnitTestEngine _unitTestEngine;
+
+        [TestInitialize]
+        public void start()
+        {
+            _unitTestEngine = new UmbracoUnitTestEngine();
+        }
+
+        [TestMethod]
+        public void EngineContentGetByIdTest()
+        {
+            var id = 1;
+
+            var content = new Mock<IContent>();
+
+            _unitTestEngine.mockServiceContext.ContentService.Setup(s => s.GetById(id)).Returns(content.Object);
+
+            var controller = new ServicesTestController(_unitTestEngine.UmbracoContext, _unitTestEngine.UmbracoHelper);
+            _unitTestEngine.RegisterController(controller);
+
+            var res = controller.ContentGetById(id);
+
+            Assert.AreSame(content.Object, res);
+        }
+
+        [TestMethod]
+        public void EngineContentSetValueTest()
+        {
+            var id = 1;
+            var prop = "testProp";
+            var value = "testVal";
+
+            var content = new Mock<IContent>();
+
+            _unitTestEngine.mockServiceContext.ContentService.Setup(s => s.GetById(id)).Returns(content.Object);
+            content.Setup(s => s.SetValue(prop, value)).Verifiable("Must set a value");
+
+            var controller = new ServicesTestController(_unitTestEngine.UmbracoContext, _unitTestEngine.UmbracoHelper);
+            _unitTestEngine.RegisterController(controller);
+
+            controller.ContentSetValue(id, prop, value);
+
+            content.Verify();
+        }
+
+        [TestMethod]
+        public void EngineContentCreateContentTest()
+        {
+            var name = "testName";
+            var alias = "testAlias";
+
+            var content = new Mock<IContent>();
+
+            _unitTestEngine.mockServiceContext.ContentService.Setup(s => s.CreateContent(name, null, alias, 0)).Returns(content.Object).Verifiable("Must create content");
+
+            var controller = new ServicesTestController(_unitTestEngine.UmbracoContext, _unitTestEngine.UmbracoHelper);
+            _unitTestEngine.RegisterController(controller);
+
+            var res = controller.ContentCreateContent(name,alias);
+
+            Assert.AreSame(content.Object, res);
+
+            _unitTestEngine.mockServiceContext.ContentService.Verify();
+        }
+
+        [TestMethod]
+        public void EngineContentSaveAndPublishTest()
+        {
+            var id = 1;
+
+            var content = new Mock<IContent>();
+
+            _unitTestEngine.mockServiceContext.ContentService.Setup(s => s.GetById(id)).Returns(content.Object);
+            _unitTestEngine.mockServiceContext.ContentService.Setup(s => s.SaveAndPublishWithStatus(content.Object, 0, true)).Verifiable("Must save");
+
+            var controller = new ServicesTestController(_unitTestEngine.UmbracoContext, _unitTestEngine.UmbracoHelper);
+            _unitTestEngine.RegisterController(controller);
+
+            controller.ContentSaveAndPublishWithStatus(id);
+
+            content.Verify();
+        }
+
+
+        [TestCleanup]
+        public void clean()
+        {
+            if (_unitTestEngine != null)
+                _unitTestEngine.Dispose();
+        }
+
+    }
+}
---
+++ SAS.Jakyl.TestWeb/Controllers/ServicesTestController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Umbraco.Core.Models;
+using Umbraco.Web;
+
+namespace SAS.Jakyl.TestWeb.Controllers
+{
+    public class ServicesTestController : Umbraco.Web.WebApi.UmbracoAuthorizedApiController
+    {
+
+        public ServicesTestController(UmbracoContext context, UmbracoHelper helper) : base(context, helper) { }
+
+        public IContent ContentGetById(int id)
+        {
+            var contentService = ApplicationContext.Services.ContentService;
+            var content = contentService.GetById(id);
+
+            return content;
+        }
+
+        public void ContentSetValue(int id, string propAlias, string propValue)
+        {
+            var contentService = ApplicationContext.Services.ContentService;
+            var content = contentService.GetById(id);
+
+            content.SetValue(propAlias, propValue);
+        }
+
+        public IContent ContentCreateContent(string name, string alias)
+        {
+            var contentService = ApplicationContext.Services.ContentService;
+            var content = contentService.CreateContent(name, null, alias);
+
+            return content;
+        }
+
+        public void ContentSaveAndPublishWithStatus(int id)
+        {
+            var contentService = ApplicationContext.Services.ContentService;
+            var content = contentService.GetById(id);
+
+            contentService.SaveAndPublishWithStatus(content);
+        }
+
+    }
+}
